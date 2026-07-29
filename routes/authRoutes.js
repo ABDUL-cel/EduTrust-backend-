fad2b88a32b93a7c32c5c35e4da8452f3fbd736f
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-07-29 07:42:30 +0100

    Update authRoutes.js

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,1 +1,20 @@
+const express = require("express");
+const router = express.Router();
 
+// Register
+router.post("/register", (req, res) => {
+    res.json({
+        success: true,
+        message: "School registration endpoint working."
+    });
+});
+
+// Login
+router.post("/login", (req, res) => {
+    res.json({
+        success: true,
+        message: "Login endpoint working."
+    });
+});
+
+module.exports = router;
