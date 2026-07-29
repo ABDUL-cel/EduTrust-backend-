9b00a4898245ab5768257996a709e97169ba98bf
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-07-29 07:32:41 +0100

    Update Server.js

--- Server.js
+++ Server.js
@@ -1,1 +1,21 @@
 
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+// Middleware
+app.use(cors());
+app.use(express.json());
+
+// Home Route
+app.get("/", (req, res) => {
+    res.send("🚀 EduTrust Backend API is Running...");
+});
+
+// Server
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
