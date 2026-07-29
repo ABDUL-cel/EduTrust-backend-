861e9ad78a542a58ca2a41195e94c525bd5fba6e
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-07-29 07:45:42 +0100

    Update authController.js

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,1 +1,14 @@
 
+exports.register = (req, res) => {
+    res.json({
+        success: true,
+        message: "School registered successfully."
+    });
+};
+
+exports.login = (req, res) => {
+    res.json({
+        success: true,
+        message: "Login successful."
+    });
+};
