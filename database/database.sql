3213534d8aab031c70a4c89b523fd1394ea2476b
ABDUL-cel <khalidabdulazeez19@gmail.com>
2026-07-29 15:16:39 +0100

    Add database.sql

--- database/database.sql
+++ database/database.sql
@@ -1,1 +1,36 @@
 
+-- ======================================
+-- EduTrust Database
+-- ======================================
+
+CREATE DATABASE IF NOT EXISTS edutrust;
+
+USE edutrust;
+
+-- ======================================
+-- Schools
+-- ======================================
+
+CREATE TABLE schools (
+
+    id INT AUTO_INCREMENT PRIMARY KEY,
+
+    school_name VARCHAR(150) NOT NULL,
+
+    owner_name VARCHAR(100) NOT NULL,
+
+    phone VARCHAR(20) NOT NULL,
+
+    email VARCHAR(100) UNIQUE NOT NULL,
+
+    address TEXT,
+
+    password VARCHAR(255) NOT NULL,
+
+    school_code VARCHAR(20) UNIQUE NOT NULL,
+
+    status ENUM('active','inactive') DEFAULT 'active',
+
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+
+);
